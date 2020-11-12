39553e32cf1f31d3502a869960345b0840b21023
lzugaj <luka.zugaj7@gmail.com>
2020-11-12 15:28:28 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/hairdresser/HairdresserServerApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.hairdresser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairdresserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HairdresserServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv2code/hairdresser/HairdresserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.hairdresser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairdresserServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
